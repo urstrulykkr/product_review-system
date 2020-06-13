451c67944d499818c43db95ca514aeb901ab5fb6
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-06-14 01:12:15 +0530

    Get rating based on query
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express"),
 	app = express(),
 	path = __dirname + "/views/",
 	bodyParser = require("body-parser"),
-	morgan = require("morgan");
+	morgan = require("morgan"),
+	{ spawn } = require("child_process");
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
@@ -21,6 +22,18 @@ app.listen(app.get("port"), () => {
 
 app.get("/", (req, res) => {
 	res.render("index");
+});
+app.post("/getRating", (req, res) => {
+	var pythonOutput;
+	const python = spawn("python", ["getRating.py", req.body.input]);
+	python.stdout.on("data", (data) => {
+		pythonOutput = data.toString();
+	});
+	python.on("close", (code) => {
+		console.log(`Python exit code: ${code}`);
+		console.log(pythonOutput);
+		res.json(pythonOutput);
+	});
 });
 app.use((req, res) => {
 	res.status(404).render("404");
