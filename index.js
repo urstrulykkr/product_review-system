b49972732910c02554aea84cba655e67c5fce06b
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2020-06-13 20:15:54 +0530

    Add Express.js backend
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express"),
+	app = express(),
+	path = __dirname + "/views/",
+	bodyParser = require("body-parser"),
+	morgan = require("morgan");
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(
+	bodyParser.urlencoded({
+		extended: false,
+	})
+);
+app.set("port", 5000);
+app.use(express.static(__dirname + "/public"));
+app.set("views", path);
+app.set("view engine", "ejs");
+app.listen(app.get("port"), () => {
+	console.log("App is running on port", app.get("port"));
+});
+
+app.get("/", (req, res) => {
+	res.render("index");
+});
+app.use((req, res, next) => {
+	res.status(404).render("404");
+});
